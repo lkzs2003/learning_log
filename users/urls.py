a35fa5f1fdcd1191a77c4fca0d3ba4150384f6c3
lsiemion <01178436@pw.edu.pl>
2024-03-26 00:54:57 +0100

    logout fix

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include, re_path
+from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
     re_path('register/', views.register, name='register'),
-    ]
+]
