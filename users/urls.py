1eb73d087a57c67b9d8598011103de7c5e619006
lsiemion <01178436@pw.edu.pl>
2024-03-26 00:12:15 +0100

    adding user registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 app_name = 'users'
 urlpatterns = [
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
-]
+    re_path('register/', views.register, name='register'),
+    ]
